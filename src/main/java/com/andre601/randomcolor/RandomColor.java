6e1668d96567cfeb0dd696eb465f7bd4696fa6b0
Andre601 <11576465+Andre601@users.noreply.github.com>
2020-07-30 00:37:14 +0200

    Version 1.2.0
    Build against PlaceholderAPI 2.10.7 and Spigot 1.16.1

--- src/main/java/com/andre601/randomcolor/RandomColor.java
+++ src/main/java/com/andre601/randomcolor/RandomColor.java
@@ -13,33 +13,21 @@ public class RandomColor extends PlaceholderExpansion {
     private final String[] COLORS = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"};
     private final String[] FORMATS = {"k", "l", "m", "n", "o"};
 
-    /**
-     * The name of the expansion, which is also used in the placeholder.
-     *
-     * @return {@code randomcolor}
-     */
+    @Override
     public String getIdentifier() {
         return "randomcolor";
     }
 
-    /**
-     * The author of the expansion.
-     *
-     * @return {@code Andre_601}
-     */
+    @Override
     public String getAuthor() {
         return "Andre_601";
     }
-
-    /**
-     * The version of the expansion.
-     *
-     * @return {@code 1.2.0}
-     */
+    
+    @Override
     public String getVersion() {
         return "1.2.0";
     }
-
+    
     private String getRandomAll(){
         String[] all = (String[])ArrayUtils.addAll(COLORS, FORMATS);
         
@@ -59,30 +47,14 @@ public class RandomColor extends PlaceholderExpansion {
     }
 
     private String getRandom(String[] values){
-        return ChatColor.COLOR_CHAR + values[random.nextInt(values.length)];
+        return ChatColor.COLOR_CHAR + values[random.nextInt(values.length)].replace("#", "x");
     }
-
-    /**
-     * This will return either a formatting/color code depending on what identifier was defined.
-     *
-     * @param  player
-     *         The {@link OfflinePlayer player} (Not really used)
-     * @param  identifier
-     *         The identifier from the placeholder ({@code %name_identifier})
-     *         <br>This should be one of those options:
-     *         <ul>
-     *             <li>{@code all} Returns a random color or formatting code. (0-9, a-f, k-o)</li>
-     *             <li>{@code format} Returns a random formatting code (k-o)</li>
-     *             <li>{@code color} Returns a random color code (0-9, a-f)</li>
-     *             <li>{@code combined} Returns a combination of a random color and formatting code (e.g. {@code §a§k})</li>
-     *             <li>{@code selected_<options>} Returns a random options that is provided.</li>
-     *         </ul>
-     *
-     * @return A random color and/or formatting code ({@code §<0-9, a-f, k-o>} depending on the selected option.
-     */
+    
+    @Override
     public String onRequest(OfflinePlayer player, String identifier){
+        identifier = identifier.toLowerCase();
 
-        switch(identifier.toLowerCase()){
+        switch(identifier){
             case "all":
                 return getRandomAll();
 
